6596f82da358e8f3307e75d49ed6d4fe77852229
Fransiskus Dave <davefransiskus@gmail.com>
2024-04-24 17:08:55 +0700

    completing chef container

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,5 +1,37 @@
+import { SubHeading } from "../../components";
+import { chef, quote, sign } from "../../assets";
+
 import "./Chef.css";
 
-const Chef = () => <div>Chef</div>;
+const Chef = () => (
+  <section className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adispiscing elit auctorsit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          auctor sit iaculis in arcu. Vulputate nulla labortis mauris eget sit.
+          Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
+          lectus eu. Congue iaculis integer curabitur semper sit nunc.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={sign} alt="sign" />
+      </div>
+    </div>
+  </section>
+);
 
 export default Chef;
