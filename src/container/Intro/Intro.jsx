75161c9aa24c0bf5a59b467951b9a7aaa3c35a32
Fransiskus Dave <davefransiskus@gmail.com>
2024-04-24 23:46:17 +0700

    completing intro container

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,5 +1,47 @@
+import { useRef, useState } from "react";
+
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../assets";
+
 import "./Intro.css";
 
-const Intro = () => <div>Intro</div>;
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prev) => !prev);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+  return (
+    <section className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      ></video>
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Intro;
