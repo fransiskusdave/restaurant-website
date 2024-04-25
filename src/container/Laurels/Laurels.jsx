8a8b556284e7ee6e6787f8ab9a09bc7d8ba63bcb
Fransiskus Dave <davefransiskus@gmail.com>
2024-04-25 23:09:58 +0700

    completing laurels container

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,5 +1,37 @@
+import { SubHeading } from "../../components";
+import { awards } from "../../constants";
+import { laurels } from "../../assets";
+
 import "./Laurels.css";
 
-const Laurels = () => <div>Laurels</div>;
+const AwardCard = ({ imgUrl, title, subtitle }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="award" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: "#dcca87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
+  </div>
+);
+
+const Laurels = () => (
+  <section className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & Recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <div className="app__laurels_awards">
+        {awards.map((award) => (
+          <AwardCard {...award} key={award.title} />
+        ))}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={laurels} alt="laurels" />
+    </div>
+  </section>
+);
 
 export default Laurels;
